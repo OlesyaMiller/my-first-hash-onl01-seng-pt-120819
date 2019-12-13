59f934d8ea6cd0abed8d36f4177c61d88baa2df5
Olesya Miller <olesyam.miller@gmail.com>
2019-12-13 00:29:35 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,6 +21,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  
   shipping_manifest["oil paintings"]
 end
 
@@ -32,11 +33,11 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+  shipping_manifest["musket"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+  shipping_manifest["gun powder"] = 4 
 
   # return the shipping_manifest hash below
 
